fb30369567752401798c614bdb80e09604896a90
Sofia Idarraga <sofia.idarraga@udea.edu.co>
2022-08-25 23:18:54 -0500

    correcting PlaylistResource annotation

--- src/main/java/ec/com/reactive/music/web/PlaylistResource.java
+++ src/main/java/ec/com/reactive/music/web/PlaylistResource.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@RestController
 public class PlaylistResource {
 
     @Autowired
